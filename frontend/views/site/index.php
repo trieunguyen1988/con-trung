fb794c1f6382d4e90829498c0f359470627c50b2
Trieu Nguyen <trieu.nguyennhu@gmail.com>
2016-04-08 10:43:11 +0700

    Test edit file

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -6,7 +6,7 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron">
+    <div class="jumbotron">kakakakakak
         <h1>Congratulations!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
